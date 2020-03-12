e48579e3f6edd9b2d7ceb3dea895565d2f497d21
Miwa Go <miwago88@gmail.com>
2020-03-12 22:31:38 +0900

    PCできた！

--- js/main.js
+++ js/main.js
@@ -1,2 +1,33 @@
 $(function(){
+    // headerの高さ分の調節
+    const headerHeight = $('#header').height();
+    $('#MV').css('padding-top', headerHeight + 'px');
+
+    // formへ飛ぶheader__contactボタン
+    $('a[href="#form"]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - headerHeight;
+        console.log(position);
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+
+    // topボタン
+    $('a[href="#"]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+
+    $('.qa__q').on('click', function () {
+        /*クリックでコンテンツを開閉*/
+        $(this).next().slideToggle(200);
+        /*矢印の向きを変更*/
+        $(this).toggleClass('open');
+    });
 });
