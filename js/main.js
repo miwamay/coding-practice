a3bccb759fdd48c577c6ce477d4c2e1f80e5e7bb
Miwa Go <miwago88@gmail.com>
2020-03-13 18:26:59 +0900

    SPやったよ～

--- js/main.js
+++ js/main.js
@@ -3,13 +3,12 @@ $(function(){
     const headerHeight = $('#header').height();
     $('#MV').css('padding-top', headerHeight + 'px');
 
-    // formへ飛ぶheader__contactボタン
+    // formへ飛ぶheader__Btnボタン
     $('a[href="#form"]').click(function(){
         var speed = 500;
         var href= $(this).attr("href");
         var target = $(href == "#" || href == "" ? 'html' : href);
         var position = target.offset().top - headerHeight;
-        console.log(position);
         $("html, body").animate({scrollTop:position}, speed, "swing");
         return false;
     });
@@ -24,6 +23,29 @@ $(function(){
         return false;
     });
 
+    //  top資料請求ボタンの高さ調節
+    //  失敗した方 / スクロールバー分のズレが生じる
+    // $(window).resize(function() {
+    //     var windowWidth = $(window).width();
+    //     if (windowWidth >= 768) {
+    //         $('#header__Btn').css('height', '');
+    //     } else {
+    //         $('#header__Btn').css('height', headerHeight);
+    //     }
+    // })
+    
+    //  上手くいった方
+    $(window).resize(function() {
+        var windowWidth = $(window).width();
+        if (wndowWidth >= 768) {
+            $('#header__Btn').css('height', '');
+        }
+        if (windowWidth < 768) {
+            $('#header__Btn').css('height', headerHeight);
+        }
+    })
+
+    // アコーディオン
     $('.qa__q').on('click', function () {
         /*クリックでコンテンツを開閉*/
         $(this).next().slideToggle(200);
